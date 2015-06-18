05eb0e25a6706e94a80b6509c3ae23bcb72a2751
Lewis Johnson <lewisj489@gmail.com>
2015-06-18 18:11:03 +0100

    New Twitter JS and CSS
    Also added email impl

--- Scripts/PageHandler.js
+++ Scripts/PageHandler.js
@@ -2,11 +2,23 @@
  * Created by lewis on 14/06/2015.
  */
 
+//Hamburger
+$(document).ready(function () {
+    $('a#hamburger').click(function ()
+    {
+        $("#page-header").toggleClass("disabled");
+    });
+});
+
 $(document).ready(function () {
 
     $("#Contact-Page").hide(0);
     $("#About-Page").hide(0);
     $("#Portfolio-Page").show(0);
+    $('#sentP').hide();
+    $("html, body").animate({
+        scrollTop: 0
+    }, 300);
 
     $('li#about-header-item').click(function ()
     {
---
+++ Scripts/Twitter.js
@@ -0,0 +1,63 @@
+/**
+ * Created by lewis on 14/06/2015.
+ */
+
+
+
+function email() {
+    var name = $('#name').val(),
+        website = $('#website').val(),
+        heard = $('#heard').val(),
+        desc = $('#desc').val(),
+        SenderEmail = $('#email').val();
+
+    $.ajax({
+        type: 'POST',
+        url: 'https://mandrillapp.com/api/1.0/messages/send.json',
+        data: {
+
+            "key": "bJv7NW-z1KY69G9pJx8_zw",
+            "message": {
+                "html": 'Name: ' + name + '\nEmail: ' + email + '\nWebsite: ' + website + '\nHow they heard: ' + heard + '\nProject Description: ' + desc,
+                "text": "Someone has contacted you.",
+                "subject": "Lewis Johnson - Contact",
+                "from_email": SenderEmail,
+                "from_name": name,
+                "to": [
+                    {
+                        "email": "lewisj489@gmail.com",
+                        "name": "Lewis Johnson",
+                        "type": "to"
+                    }
+                ]
+            }
+        }
+    }).done(function (response) {
+        console.log(response);
+        $('#sentP').show(500);
+
+    });
+}
+
+!TwitterTweetWidget(document, 'script', 'twitter-wjs');
+function TwitterTweetWidget(d, s, id) {
+    var fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ? 'http' : 'https';
+    if (!d.getElementById(id)) {
+        var js = d.createElement(s);
+        js.id = id;
+        js.src = p + '://platform.twitter.com/widgets.js';
+        fjs.parentNode.insertBefore(js, fjs);
+    }
+}
+
+
+!TwitterFeedWidget(document, "script", "twitter-wjs");
+function TwitterFeedWidget(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ? 'http' : 'https';
+    if (!d.getElementById(id)) {
+        js = d.createElement(s);
+        js.id = id;
+        js.src = p + "://platform.twitter.com/widgets.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }
+}
